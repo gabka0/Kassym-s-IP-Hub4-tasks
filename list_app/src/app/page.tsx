82d8abd9ede0525053eb5a2418ea293ab621b615
gabka0 <kassymgabdushev@gmail.com>
2025-04-20 22:22:18 +0500

    Fix

--- list_app/src/app/page.tsx
+++ list_app/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-4">Kassym's To-Do List</h1>
+      <h1 className="text-3xl font-bold mb-4">Kassym&apos;s To-Do List</h1>
       
       <div className="flex gap-2 mb-6">
         <input
